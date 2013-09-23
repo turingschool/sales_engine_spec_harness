01c1ceeb25a743fdfbfd7119d89d6753a2d16e68
Katrina Owen <katrina.owen@gmail.com>
2013-09-23 09:38:09 -0600

    Update extension specs per new requirements

--- spec/extensions/invoice_extension_spec.rb
+++ spec/extensions/invoice_extension_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe SalesEngine::Invoice, invoice: true do
+describe "SalesEngine invoice extensions", invoice: true do
   context "extensions" do
     describe ".pending" do
       it "returns Invoices without a successful transaction" do
-        invoice = SalesEngine::Invoice.find_by_id(13)
-        pending_invoice = SalesEngine::Invoice.pending
+        invoice = engine.invoice_repository.find_by_id(13)
+        pending_invoice = engine.invoice_repository.pending
 
         pending_invoices.map(&:id).include?(invoice).should == true
       end
@@ -13,25 +13,25 @@ describe SalesEngine::Invoice, invoice: true do
 
     describe ".average_revenue" do
       it "returns the average of the totals of each invoice" do
-        SalesEngine::Invoice.average_revenue.should == BigDecimal("12369.53")
+        engine.invoice_repository.average_revenue.should == BigDecimal("12369.53")
       end
     end
 
     describe ".average_revenue(date)" do
       it "returns the average of the invoice revenues for that date" do
-        SalesEngine::Invoice.average_revenue(Date.parse("March 17, 2012")).should == BigDecimal("11603.14")
+        engine.invoice_repository.average_revenue(Date.parse("March 17, 2012")).should == BigDecimal("11603.14")
       end
     end
 
     describe ".average_items" do
       it "returns the average of the number of items for each invoice" do
-        SalesEngine::Invoice.average_items.should == BigDecimal("24.45")
+        engine.invoice_repository.average_items.should == BigDecimal("24.45")
       end
     end
 
     describe ".average_items(date)" do
       it "returns the average of the invoice items for that date" do
-        SalesEngine::Invoice.average_items(Date.parse("March 21, 2012")).should == BigDecimal("24.29")
+        engine.invoice_repository.average_items(Date.parse("March 21, 2012")).should == BigDecimal("24.29")
       end
     end
   end
--- spec/extensions/merchant_extension_spec.rb
+++ spec/extensions/merchant_extension_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe SalesEngine::Merchant, merchant: true do
+describe "SalesEngine merchant extensions", merchant: true do
   context "extensions" do
     describe ".dates_by_revenue" do
       it  "returns an array of Dates in descending order of revenue" do
-        dates = SalesEngine::Merchant.dates_by_revenue
+        dates = engine.merchant_repository.dates_by_revenue
 
         (dates.first == DateTime.parse("2012-03-09") || dates.first == Date.parse("2012-03-09")).should be_true
         (dates[21] == DateTime.parse("2012-03-06") || dates[21] == Date.parse("2012-03-06")).should be_true
@@ -13,7 +13,7 @@ describe SalesEngine::Merchant, merchant: true do
 
     describe ".dates_by_revenue(x)" do
       it  "returns the top x Dates in descending order of revenue" do
-        dates = SalesEngine::Merchant.dates_by_revenue(5)
+        dates = engine.merchant_repository.dates_by_revenue(5)
 
         dates.size.should == 5
         (dates[1] == DateTime.parse("2012-03-08") || dates[1] == Date.parse("2012-03-08")).should be_true
@@ -25,7 +25,7 @@ describe SalesEngine::Merchant, merchant: true do
       it "returns the total revenue for all merchants across several dates" do
         date_1 = Date.parse("2012-03-14")
         date_2 = Date.parse("2012-03-16")
-        revenue = SalesEngine::Merchant.revenue(date_1..date_2)
+        revenue = engine.merchant_repository.revenue(date_1..date_2)
 
         revenue.should == BigDecimal("8226179.74")
       end
@@ -35,7 +35,7 @@ describe SalesEngine::Merchant, merchant: true do
       it "returns the total revenue for that merchant across several dates" do
         date_1 = Date.parse("2012-03-01")
         date_2 = Date.parse("2012-03-07")
-        merchant = SalesEngine::Merchant.find_by_id(7)
+        merchant = engine.merchant_repository.find_by_id(7)
         revenue = merchant.revenue(date_1..date_2)
 
         revenue.should == BigDecimal("57103.77")
